536a0fe08fb91b04737c5ad34698ef48fc7c4287
James Clarke <james@clarkezone.net>
2022-04-21 01:49:07 -0700

    version command (#2)

---
+++ cmd/version.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/clarkezone/previewd/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+// Show current version
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "Show previewd version",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Printf("rk version %s hash %s\n", config.VersionString, config.VersionHash)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCommand)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runwebhookserverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runwebhookserverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
