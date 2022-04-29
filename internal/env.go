e3b4ac1b7993dbae8b32493e5114af0897c9fcce
James Clarke <james@clarkezone.net>
2022-04-29 15:40:29 +0000

    Fix default log level

--- internal/env.go
+++ internal/env.go
@@ -13,18 +13,24 @@ const (
 	PortVar     = "port"
 	defaultPort = 8090
 	// LogLevelVar is name of environment variable containing loglevel
-	LogLevelVar = "loglevel"
+	LogLevelVar     = "loglevel"
+	defaultLogLevel = "Warn"
 )
 
 var (
 	// Port is the port set in environment
 	Port int
+
+	// LogLevel is read from env
+	LogLevel string
 )
 
 func init() {
 	viper.AutomaticEnv()
 	viper.SetDefault(PortVar, defaultPort)
+	viper.SetDefault(LogLevelVar, defaultLogLevel)
 	Port = viper.GetInt(PortVar)
+	LogLevel = viper.GetString(LogLevelVar)
 }
 
 // ValidateEnv validates environment variables
