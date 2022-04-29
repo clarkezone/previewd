c24fa3cb8bd2f580d4a0714412939935506e9331
James Clarke <james@clarkezone.net>
2022-04-29 15:05:27 +0000

    Fix version command

--- cmd/version.go
+++ cmd/version.go
@@ -1,21 +1,14 @@
 package cmd
 
 import (
-	clarkezoneLog "github.com/clarkezone/previewd/pkg/log"
+	"fmt"
 
 	"github.com/clarkezone/previewd/pkg/config"
 	"github.com/spf13/cobra"
 )
 
 // Show current version
-var versionCommand = &cobra.Command{
-	Use:   "version",
-	Short: "Show previewd version",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		clarkezoneLog.Infof("rk version:%s hash:%s\n", config.VersionString, config.VersionHash)
-		return nil
-	},
-}
+var versionCommand = getVersionCommand()
 
 func init() {
 	rootCmd.AddCommand(versionCommand)
@@ -29,4 +22,18 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// runwebhookserverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func getVersionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Show previewd version",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			_, err := fmt.Fprintf(cmd.OutOrStdout(), "rk version:%s hash:%s\n", config.VersionString, config.VersionHash)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
 }
