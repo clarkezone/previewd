1bec1b4f25db0f22318e80431cc47cda323ae870
James Clarke <james@clarkezone.net>
2022-05-09 23:37:12 +0000

    Bucket by responsecode

--- pkg/basicserver/middlewaremetrics.go
+++ pkg/basicserver/middlewaremetrics.go
@@ -1,6 +1,7 @@
 package basicserver
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,28 +10,39 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+type statusRecorder struct {
+	http.ResponseWriter
+	code int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.code = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
 // PromMetricsMiddleware adds simple prometheus metrics type PromMetricsMiddleware
 type PromMetricsMiddleware struct {
 	handler         http.Handler
-	opsProcessed    prometheus.Counter
+	opsProcessed    *prometheus.CounterVec
 	requestDuration *prometheus.HistogramVec
 }
 
 func (l *PromMetricsMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rec := statusRecorder{w, 200}
 	start := time.Now()
 	l.handler.ServeHTTP(w, r)
 	httpDuration := time.Since(start)
-	l.opsProcessed.Inc()
+	l.opsProcessed.WithLabelValues(fmt.Sprint(rec.code), r.Method).Inc()
 	l.requestDuration.WithLabelValues(r.URL.RawPath).Observe(httpDuration.Seconds())
 }
 
 func newMiddleware(handlerToWrap http.Handler) *PromMetricsMiddleware {
 	mw := PromMetricsMiddleware{}
 	mw.handler = handlerToWrap
-	mw.opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+	mw.opsProcessed = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "previewd_testserver_totalops",
 		Help: "The total number of processed http requests for testserver",
-	})
+	}, []string{"responsecode", "method"})
 	mw.requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "previewd_testserver_duration_seconds",
 		Help:    "Histogram of duration in seconds",
