24584230a64962e39651caa65a6dbb2fc1b4058a
James Clarke <james@clarkezone.net>
2022-05-11 02:48:16 +0000

    fix linting

--- cmd/runwebhookserver.go
+++ cmd/runwebhookserver.go
@@ -40,22 +40,23 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		clarkezoneLog.Infof("runwebhookserver called")
+
+		//	if serve || initialbuild || webhooklisten || initialclone {
+		//		result := verifyFlags(repo, localRootDir, initialbuild, initialclone)
+		//		if result != nil {
+		//			return result
+		//		}
+		//	} else {
+		//		return nil
+		//	}
 		// TODO args from flags
 		// err := PerformActions(repo, localRootDir, initalBranchName, incluster, "jekyllpreviewv2", webhooklisten)
 	},
 }
 
+// PerformActions runs the webhook logic
 func PerformActions(repo string, localRootDir string, initialBranch string,
 	preformInCluster bool, namespace string, webhooklisten bool, serve bool, initialbuild bool, initialclone bool) error {
-	if serve || initialbuild || webhooklisten || initialclone {
-		result := verifyFlags(repo, localRootDir, initialbuild, initialclone)
-		if result != nil {
-			return result
-		}
-	} else {
-		return nil
-	}
-
 	sourceDir := path.Join(localRootDir, "sourceroot")
 	fileinfo, res := os.Stat(sourceDir)
 	if fileinfo != nil && res == nil {
@@ -80,13 +81,10 @@ func PerformActions(repo string, localRootDir string, initialBranch string,
 	whl = webhooklistener.CreateWebhookListener(lrm)
 
 	if initialclone {
-		err := lrm.InitialClone(repo, "")
+		err = initialClone(repo, initialBranch)
 		if err != nil {
+			clarkezoneLog.Debugf("initialClone failed %v", err)
 			return err
-		}
-
-		if initialBranch != "" {
-			return lrm.SwitchBranch(initialBranch)
 		}
 	}
 
@@ -95,32 +93,47 @@ func PerformActions(repo string, localRootDir string, initialBranch string,
 	}
 
 	if initialbuild {
-		notifier := (func(job *batchv1.Job, typee jobmanager.ResourseStateType) {
-			log.Printf("Got job in outside world %v", typee)
-
-			if typee == jobmanager.Update && job.Status.Active == 0 && job.Status.Failed > 0 {
-				log.Printf("Failed job detected")
-			}
-		})
-		var imagePath string
-		fmt.Printf("%v", runtime.GOARCH)
-		if runtime.GOARCH == "amd64" {
-			imagePath = "registry.hub.docker.com/clarkezone/jekyllbuilder:0.0.1.8"
-		} else {
-
-			imagePath = "registry.dev.clarkezone.dev/jekyllbuilder:arm"
-		}
-		command := []string{"sh", "-c", "--"}
-		params := []string{"cd source;bundle install;bundle exec jekyll build -d /site JEKYLL_ENV=production"}
-		_, err = jm.CreateJob("jekyll-render-container", namespace, imagePath, command, params, notifier)
-		if err != nil {
-			log.Printf("Failed to create job: %v\n", err.Error())
-		}
-
+		initialBuild(namespace)
 	}
 	return nil
 }
 
+func initialClone(repo string, initialBranch string) error {
+	err := lrm.InitialClone(repo, "")
+	if err != nil {
+		return err
+	}
+
+	if initialBranch != "" {
+		return lrm.SwitchBranch(initialBranch)
+	}
+	return nil
+}
+
+func initialBuild(namespace string) {
+	notifier := (func(job *batchv1.Job, typee jobmanager.ResourseStateType) {
+		log.Printf("Got job in outside world %v", typee)
+
+		if typee == jobmanager.Update && job.Status.Active == 0 && job.Status.Failed > 0 {
+			log.Printf("Failed job detected")
+		}
+	})
+	var imagePath string
+	fmt.Printf("%v", runtime.GOARCH)
+	if runtime.GOARCH == "amd64" {
+		imagePath = "registry.hub.docker.com/clarkezone/jekyllbuilder:0.0.1.8"
+	} else {
+		imagePath = "registry.dev.clarkezone.dev/jekyllbuilder:arm"
+	}
+	command := []string{"sh", "-c", "--"}
+	params := []string{"cd source;bundle install;bundle exec jekyll build -d /site JEKYLL_ENV=production"}
+	_, err := jm.CreateJob("jekyll-render-container", namespace, imagePath, command, params, notifier)
+	if err != nil {
+		log.Printf("Failed to create job: %v\n", err.Error())
+	}
+}
+
+//nolint
 func verifyFlags(repo string, localRootDir string, build bool, clone bool) error {
 	return nil
 	//	if clone && repo == "" {
