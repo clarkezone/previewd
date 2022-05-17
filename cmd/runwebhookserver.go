6526b77eb0b242bbbb037cb48890b7d8f23457ef
James Clarke <james@clarkezone.net>
2022-05-17 14:56:15 +0000

    Add initial flag

--- cmd/runwebhookserver.go
+++ cmd/runwebhookserver.go
@@ -13,15 +13,20 @@ import (
 
 // runwebhookserverCmd represents the runwebhookserver command
 var runwebhookserverCmd = &cobra.Command{
-	Use:   "runwebhookserver",
+	Use:   "runwebhookserver http://repotoclone.git",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
+previewd runwebhookserver http://repo.git --localdir /tmp/foo
+`,
+	Args: cobra.ExactArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		clarkezoneLog.Infof("PreRunE with args: %v", args)
+		return nil
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	},
 	Run: func(cmd *cobra.Command, args []string) {
+
 		clarkezoneLog.Infof("runwebhookserver called")
 	},
 }
@@ -29,13 +34,26 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(runwebhookserverCmd)
 
+	runwebhookserverCmd.Flags().StringP("localdir", "d", "", "absolute path to local dir to clone into")
+
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
+	// and all subcommands, e.g.:jj
 	// runwebhookserverCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// runwebhookserverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// if serve || initialbuild || webhooklisten || initialclone {
+	//		result := verifyFlags(repo, localRootDir, initialbuild, initialclone)
+	//		if result != nil {
+	//			return result
+	//		}
+	//	} else {
+	//		return nil
+	//	}
+	// TODO args from flags
+	// err := PerformActions(repo, localRootDir, initalBranchName, incluster, "jekyllpreviewv2", webhooklisten)
 }
